b1e7a33326895ffb7591935c51a564cabb87a832
Infant Frontender <newbornfrontender@gmail.com>
2018-11-05 20:48:22 +0300

    Завершил 18 лекцию

--- README.md
+++ README.md
@@ -29,7 +29,7 @@
 15. Arrays and How to create Arrays in JavaScript (✓)
 16. Useful Methods when working with Arrays (✓)
 17. Remove Elements from an Array (✓)
-18. Sorting Arrays and Other Methods (...)
+18. Sorting Arrays and Other Methods (✓)
 19. JavaScript Objects (...)
 20. Const Variables with Arrays & Objects (...)
 21. JavaScript Functions - Function Declaration (...)
--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const months = new Array('January', 'February', 'March', 'April', 'May', 'June');
+// const months = new Array('January', 'February', 'March', 'April', 'May', 'June');
 
-months.pop();
-months.shift();
-months.splice(2, 1);
+// console.log(months.sort().reverse());
 
-console.log(months);
+// const arr1 = ['1', '2', '3'];
+// const arr2 = ['7', '8', '9'];
+
+// console.log(arr1.concat(arr2));
+
+const nums = [1, 82, 31, 47, 4, 40, 0, 16, 56, 77]
+
+function sortLowerNums(num1, num2) {
+  return num1 - num2;
+};
+
+function sortGreaterNums(num1, num2) {
+  return num2 - num1;
+};
+
+console.log(nums.sort(sortLowerNums));
+console.log(nums.sort(sortGreaterNums));
