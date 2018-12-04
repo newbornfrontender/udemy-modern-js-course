0dbdf8cf563945d7d6fd1d8314a0bc45bb32fc23
Infant Frontender <newbornfrontender@gmail.com>
2018-12-04 18:16:34 +0300

    Завершил 33 лекцию

--- README.md
+++ README.md
@@ -44,7 +44,7 @@
 30. For Loops in JavaScript (✓)
 31. For Loops in JavaScript: Looping an Array (✓)
 32. While & Do While Loops (✓)
-33. for, forEach & Maps (...)
+33. for, forEach & Maps (✓)
 34. Looping Objects in JavaScript (...)
 35. Try - Catch - Finally in JavaScript (...)
 36. The WIndow Object - Part 1 (...)
--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const products = ['Product 1', 'Product 2', 'Product 3'];
+// const todo = ['Homework', 'Food', 'Project'];
 
-let i = 0;
+// for (let i = 0; i < todo.length; i++) {
+//   console.log(todo[i]);
+// }
 
-do {
-  console.log(products[i]);
+// todo.forEach((assigment, index) => console.log(`${index + 1}: ${assigment}`))
 
-  i++;
-} while (i < products.length);
+const shoppingCart = [
+  {
+    id: 1,
+    product: 'Book',
+  },
+  {
+    id: 2,
+    product: 'Shirt',
+  },
+  {
+    id: 3,
+    product: 'Album',
+  },
+];
+
+// const productName = (cart) =>
+//   shoppingCart.map(({ id, product }) => console.log(`${id}: ${product}`));
+
+// productName(shoppingCart);
+
+const productName = shoppingCart.map(({ product }) => {
+  return product;
+});
+
+console.log(productName);
