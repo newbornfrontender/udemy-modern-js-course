154741feeb322d5b54f580c7481d367ec7f7b188
Infant Frontender <newbornfrontender@gmail.com>
2018-12-14 21:49:50 +0300

    Завершил 40 лекцию

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let element;
+
+element = document
+element = document.all;
+element = document.all[20];
+element = document.head;
+element = document.body;
+element = document.domain;
+element = document.URL;
+element = document.characterSet;
+element = document.contentType;
+
+// Links
+element = document.links;
+element = document.links[2];
+element = document.links[2].id;
+element = document.links[2].className;
+element = document.links[2].classList;
+
+console.log(element);
